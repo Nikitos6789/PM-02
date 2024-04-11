76b235b73271dfdbf98b85f01773531a89f874f1
Габов Никита Евгеньевич <20007@iat.iat>
2024-04-11 11:05:03 +0800

    Module PM-02

---
+++ project/PM-02/database/migrations/2024_04_10_050735_create_productions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productions', function (Blueprint $table) {
+            $table->id();
+            $table->string('airplane_model');
+            $table->date('date_production');
+            $table->foreignId('employee_id')->constrained('employees')->onDelete('cascade');
+            $table->foreignId('material_id')->constrained('materials')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productions');
+    }
+};
